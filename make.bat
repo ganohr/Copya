80dcbc396ccb3567b9c5a7246f4e867802529cab
ganohr <49603804+ganohr@users.noreply.github.com>
2021-05-18 14:58:07 +0900

    remove .md

--- make.bat
+++ make.bat
@@ -1,9 +1,6 @@
 @echo off
 setlocal
 
-rem This batch file is runnable over Win10.
-
-rem get version
 for /f "delims=" %%a in (version.txt) do (
 	set "version=%%a"
 )
@@ -11,22 +8,19 @@ for /f "delims=" %%a in (version.txt) do (
 echo VERSION
 echo %version%
 
-rem set output path
 set "outpath=.\trunk\%version%"
 mkdir %outpath%\
 
-copy *.md %outpath%\
+rem copy *.md %outpath%\
 copy *.txt %outpath%\
 copy *.php %outpath%\
 
 cd %outpath%
 
-rem create zip
 set "zipfile=..\..\release\copya-%version%.zip"
 del %zipfile%
 
 tar -a -c -f %zipfile% *
-dir %zipfile%
 
 endlocal
 pause
